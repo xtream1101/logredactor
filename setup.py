1a4b4f0cf9f585587828a85991192841966647cb
Eddy Hintze <eddy@hintze.co>
2023-02-10 09:17:11 -0500

    Changed setup.py to use double quotes

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 try:
-    with open('README.md', 'r') as f:
+    with open("README.md", "r") as f:
         long_description = f.read()
 except FileNotFoundError:
-    long_description = ''
+    long_description = "
 
 setup(
-    name='logredactor',
+    name="logredactor",
     packages=find_packages(),
-    version='0.0.2',
-    url='https://github.com/xtream1101/logredactor',
-    description='Redact logs based on regex filters',
+    version="0.0.2",
+    url="https://github.com/xtream1101/logredactor",
+    description="Redact logs based on regex filters",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
 )
